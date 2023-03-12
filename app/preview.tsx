bebcc1aae4b2bc38e7033e433f469f363c49810c
Brian Chen <brichen95@gmail.com>
2023-03-12 00:21:20 -0500

    janky loading symbol

--- app/preview.tsx
+++ app/preview.tsx
@@ -21,58 +21,60 @@ export default function Preview({file, ffmpeg}) {
     'mp4',
   ]
   const FFMPEG_IMG_FORMATS = [
+    'ico',
     'jpg',
     'png',
     'png-white-to-transparent'
   ]
 
   if (file) {
-      return (
-        <div>
-          <div className='flex h-500'>
-            <div className='flex-2 pr-5'>
-              { IMGS.includes(file.type)
-                ? <Image width="500" height="500" alt="n/a" src={URL.createObjectURL(file)} />
-                : (VIDS.includes(file.type) || file.name.includes(".mkv"))
-                  ? <video controls width="500" src={URL.createObjectURL(file)} />
-                  : <div>Invalid file type</div>
-              }
-            </div>
-            <div className='flex-1'>
-              { IMGS.includes(file.type)
+    console.log(file)
+    return (
+      <div>
+        <div className='flex h-500'>
+          <div className='flex-2 pr-5'>
+            { IMGS.includes(file.type)
+              ? <Image width="500" height="500" alt="n/a" src={URL.createObjectURL(file)} />
+              : (VIDS.includes(file.type) || file.name.includes(".mkv"))
+                ? <video controls width="500" src={URL.createObjectURL(file)} />
+                : <div>Invalid file type</div>
+            }
+          </div>
+          <div className='flex-1'>
+            { IMGS.includes(file.type)
+              ? (
+                <div>
+                  <p>Type: {file.type}</p>
+                  <p>Size: {(file.size / 1000 / 1000).toFixed(2)} GB</p>
+                </div>
+              )
+              : (VIDS.includes(file.type) || file.name.includes(".mkv"))
                 ? (
                   <div>
-                    <p>Type: {file.type}</p>
-                    <p>Size: {(file.size / 1000 / 1000).toFixed(2)} GB</p>
+                    <p>Type: {file.type ? file.type : file.name}</p>
+                    <p>Size: {(file.size / 1000 / 1000).toFixed(2)} MB</p>
                   </div>
                 )
-                : (VIDS.includes(file.type) || file.name.includes(".mkv"))
-                  ? (
-                    <div>
-                      <p>Type: {file.type ? file.type : file.name}</p>
-                      <p>Size: {(file.size / 1000 / 1000).toFixed(2)} MB</p>
-                    </div>
-                  )
+                : (
+                  <div></div>
+                )
+            }
+          </div>
+        </div>
+        <div className='pt-3'>
+          <div className='flex'>
+              { IMGS.includes(file.type)
+                ? <Converter type='image' options={FFMPEG_IMG_FORMATS} ffmpeg={ffmpeg} file={file}/>
+                  : (VIDS.includes(file.type) || file.name.includes(".mkv"))
+                  ? <Converter type='video' options={FFMPEG_VIDEO_FORMATS} ffmpeg={ffmpeg} file={file}/>
                   : (
                     <div></div>
                   )
-              }
-            </div>
-          </div>
-          <div className='pt-3'>
-            <div className='flex'>
-                { IMGS.includes(file.type)
-                  ? <Converter type='image' options={FFMPEG_IMG_FORMATS} ffmpeg={ffmpeg} file={file}/>
-                   : (VIDS.includes(file.type) || file.name.includes(".mkv"))
-                    ? <Converter type='video' options={FFMPEG_VIDEO_FORMATS} ffmpeg={ffmpeg} file={file}/>
-                    : (
-                      <div></div>
-                    )
-                }          
-            </div>
+              }          
           </div>
         </div>
-      )
+      </div>
+    )
   } else return (
     <div></div>
   )
